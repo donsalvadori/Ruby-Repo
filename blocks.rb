069bcec32e6be883dba901b178561042069baea8
chris salvi <csalvi42@gmail.com>
2014-08-11 06:34:54 -0700

    Update blocks.rb
    more practice with blocks and scoping

--- blocks.rb
+++ blocks.rb
@@ -29,3 +29,42 @@ puts "this is the first line of our ruby program"
 BEGIN { puts "INSIDE OF THE BEGIN BLOCK"}
 END { puts "INSIDE OF THE END BLOCK"}
 
+#New material
+#Since August 11
+def say_hello2(&block)
+	puts "Hello, world"
+	yield
+	puts "I said hello"
+end
+
+say_hello2 {
+	puts "I am inside of the say_hello2 block"
+}
+
+#0-9
+
+def show_numbers(numbers=10)
+	i=10
+	while i < 10
+		yield i+=1
+	end
+end
+
+show_numbers do |number|
+	puts "The current number is #{number}."
+	number *= 2
+	puts "the current number * 2 is #{number}."
+end
+
+def get_name (&block)
+	print "enter your name: "
+	name= gets.chomp
+	yield name
+	puts "it's nice to meet you, #{name}."
+end
+
+get_name do |name|
+	puts "Hi, #{name}."
+	name.name.upcase
+	puts "Uppercased, your name is #{name}."
+end
