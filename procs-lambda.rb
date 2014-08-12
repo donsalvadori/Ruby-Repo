5c2f87c1864f2a31b9dee61ef6f89800effcbcd8
chris salvi <csalvi42@gmail.com>
2014-08-12 08:56:25 -0700

    Update procs&lambda

--- procs-lambda.rb
+++ procs-lambda.rb
@@ -18,3 +18,29 @@ my_lambda = -> {}
 
 #ammend real code here
 
+my_proc = proc { |name| puts "Hello, #{name}"}
+
+my_proc.call "Chris"
+
+
+my_lambda = lambda {|name| puts "Hello, #{name}"}
+
+my_lambda.call "chris"
+
+#so lambdas require an argument where procs dont
+
+def return_from_proc
+variable= proc { return "returning from proc inside method"}
+variable.call
+return "returning from proc as last line from method"
+end
+
+def return_from_lambda
+variable= lambda { return "returning from lambda inside method"}
+variable.call
+return "returning from lambda as last line from method"
+end
+
+puts return_from_proc
+
+puts return_from_lambda
