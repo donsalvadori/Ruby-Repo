71b7bb503724e16eb645d9cfdc92e6dea9a2eecb
chris salvi <csalvi42@gmail.com>
2014-08-12 08:22:51 -0700

    practice with procs and lambdas in ruby
    see title

---
+++ procs-lambda.rb
@@ -0,0 +1,20 @@
+#creating procs & lambda practice test
+
+my_proc= Proc.new{}
+
+my_proc.call
+
+my_proc = proc {}
+
+my_proc = pro {puts "Hello World"}
+
+my_proc.call
+
+#lambda
+
+my_lambda = lambda{ }
+
+my_lambda = -> {}
+
+#ammend real code here
+
