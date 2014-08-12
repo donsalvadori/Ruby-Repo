32a013d2c6974a53e8b0e88cf1b852353714cdd3
chris salvi <csalvi42@gmail.com>
2014-08-12 09:01:24 -0700

    Update paper.rb

--- paper.rb
+++ paper.rb
@@ -1,37 +1,38 @@
-#class paper
+#class paper: Prints a title, heading, and body input by user
 
 class Paper
-def initialize(&block)
-yield self if block_given?
+  def initialize(&block)
+  yield self if block_given?
 end
 
 def set_variable
-return Proc.new do |kind,val|
-[kind, val].join(": ")
-end
+  return Proc.new do |kind,val|
+  [kind, val].join(": ")
+  end
 end
 
 def title (value)
-@title = set_variable.call "TITLE", value
+  @title = set_variable.call "TITLE", value
 end
 
 def heading(value)
-@heading =set_variable.call "HEADING", value
+  @heading =set_variable.call "HEADING", value
 end
 
 def body (value)
-@body = set_variable.call "BODY", value
+  @body = set_variable.call "BODY", value
 end
 
 def display
-puts @title
-puts "------------------"
-puts @ heading
-puts "------------------"
-puts @body
-puts "------------------"
-end
-end
+  puts @title
+  puts "------------------"
+  puts @ heading
+  puts "------------------"
+  puts @body
+  puts "------------------"
+  end
+
+end #end of class
 
 paper = Paper.new do |p|
 p.title "my awesome paper"
