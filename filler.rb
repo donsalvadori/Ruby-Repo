ac402f13bf9e55a62e7ba1c099845d5d788c6337
chris salvi <csalvi42@gmail.com>
2014-10-26 21:11:42 -0700

    Update filler.rb

--- filler.rb
+++ filler.rb
@@ -6,7 +6,6 @@
 #alas we share our love for ruby
 #and he will help me stay motivated, virtuality be damned
 #Michael Hartel RoR book relased today 14th of october
-#read chapters 1 and 2
 #nothing new to learn, but good refresher and well written
 #looking forward to getting into meater part of book
 #finish reading the whole book before December
