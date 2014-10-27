9d288e03e82a5e627cb36058b6668e64ed55cafb
chris salvi <csalvi42@gmail.com>
2014-10-26 21:11:20 -0700

    Update filler.rb

--- filler.rb
+++ filler.rb
@@ -9,7 +9,6 @@
 #read chapters 1 and 2
 #nothing new to learn, but good refresher and well written
 #looking forward to getting into meater part of book
-#hoping v3 is a significant advancement
 #finish reading the whole book before December
 # Ruby will be my life in 2015 and onward. Will be employable in the field
 
